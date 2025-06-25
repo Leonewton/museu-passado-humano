5019639739f838b5405dd15a64bbc198593a5c20
Thiago Moreira <56270443+ThiagoVMM@users.noreply.github.com>
2025-06-25 12:09:17 -0300

    Update newsletter.js

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,7 +1,20 @@
 function cadastrarNewsletter(evento) {
   evento.preventDefault();
 
-  alert("E-mail cadastrado com sucesso!");
+  const email = document.getElementById("email-footer").value;
 
-  document.getElementById("email-footer").value = null;
+  const novaMensagemRef = banco.ref("emails-newsletter").push();
+
+  novaMensagemRef
+    .set({
+      email: email,
+    })
+    .then(() => {
+      alert("E-mail cadastrado com sucesso!");
+      document.getElementById("email-footer").value = "";
+    })
+    .catch((error) => {
+      console.error("Erro ao cadastrar e-mail:", error);
+      alert("Erro ao cadastrar e-mail. Veja o console para mais detalhes.");
+    });
 }
